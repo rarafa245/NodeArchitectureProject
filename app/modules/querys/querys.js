3049c87ed12f1e22483fbf614f882d17b8949a61
Rafael <rarafa245@gmail.com>
2020-07-02 04:04:34 -0300

    fixing_Sequelize

--- app/modules/querys/querys.js
+++ app/modules/querys/querys.js
@@ -2,7 +2,7 @@ const tables = require('../tables')
 
 async function queryAll() {
   const elements = await tables.Postagens.findAll();
-  console.log("All users:", JSON.stringify(elements, null, 7));
+  console.log(elements[0].dataValues)
 }
 
 module.exports = queryAll
